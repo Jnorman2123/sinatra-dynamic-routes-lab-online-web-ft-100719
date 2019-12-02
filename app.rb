7308bfca1a2efc7cf2691e3be429f9887245b83d
Jonathan Norman <jnorman2123@gmail.com>
2019-12-02 14:54:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,39 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{@name.reverse}"
+    @reverse_name = params[:name].reverse
+    "#{@reverse_name}"
   end
 
   get '/square/:number' do
-    @num = params[:number].to_i ** 2
-    "#{@num.to_s}"
+    @square_number = params[:number].to_i ** 2
+    "#{@square_number}"
   end
 
   get '/say/:number/:phrase' do
-    @phrase = []
-    @num = params[:number].to_i
-    @num.times {@phrase << params[:phrase]}
-    "#{@phrase.join(", ")}"
-  end
-
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} " + "#{params[:word2]} " + "#{params[:word3]} " + "#{params[:word4]} " + "#{params[:word5]}."
-  end
-
-  get '/:operation/:number1/:number2' do
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
-    @operation = params[:operation]
-    case @operation
-    when "add"
-      "#{@num1 + @num2}.to_s"
-    when "subtract"
-      "#{@num1 - @num2}.to_s"
-    when "multiply"
-      "#{@num1 * @num2}.to_s"
-    when "divide"
-      "#{@num1 / @num2}.to_s"
-    end
+    params[:number].to_i.times {print params[:phrase]}
   end
 end
